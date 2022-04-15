6c03b8daf7e175521e67ff07d473d2c6b6cb8c14
brijsiyag <brijsiyag@gmail.com>
2022-04-15 15:43:35 +0530

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-
+import Navbar from "./Header/Navbar";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
