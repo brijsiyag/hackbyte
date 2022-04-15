b12b15029658ad28b254c8c18a5d758dae6f2e63
optimm <ayushsaxena823@gmail.com>
2022-04-15 16:48:28 +0530

    login resolves

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import Navbar from "./Header/Navbar";
+import Login from "./Body/Login/Login";
+
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Login/>
     </div>
   );
 }
